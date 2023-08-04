7459b34c9b52ba8dccf0f4b3c700564fcff2cfc8
theDAVID543 <theDAVID5432@gmail.com>
2023-08-04 13:30:33 +0800

    init

---
+++ src/main/java/thedavid/antistringdupe/AntiStringDupe.java
@@ -0,0 +1,30 @@
+package thedavid.antistringdupe;
+
+import io.papermc.paper.event.block.BlockBreakBlockEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiStringDupe extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    @EventHandler
+    public void onWaterBreakString(BlockBreakBlockEvent e){
+        if(e.getBlock().getType() == Material.TRIPWIRE){
+            e.getBlock().setType(Material.AIR);
+        }
+    }
+}
